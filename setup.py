b974451b5504512ec0561135d0748017df6db2b8
daniel-layton-wright <daniel.layton.wright@gmail.com>
2024-04-20 14:55:27 -0700

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='nanogpt',
+    version='0.0.1',
+    description='Use deep learning to train language models',
+    url='https://github.com/daniel-layton-wright/nanogpt.git',
+    author='Daniel Wright',
+    author_email='dlwright@alumni.stanford.edu',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    package_data={
+        'nanogpt': [
+        ]
+    },
+    install_requires=[
+        'torch', 'numpy', 'lightning', 'hydra-core'
+    ],
+    tests_require=[],
+    setup_requires=[],
+    zip_safe=True,
+)
